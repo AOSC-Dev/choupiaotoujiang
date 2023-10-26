2a1c12277711a75af838c5aa74e3b512e4ca3e4a
eatradish <sakiiily@aosc.io>
2023-10-26 11:13:02 +0800

    chore: use `fastrand` crate
    - Add argument range

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
-use std::{
-    io::stdin,
-    time::Instant,
-};
+use std::{io::stdin, time::Instant};
 
 use clap::Parser;
 use indicatif::ProgressBar;
-use rand::seq::SliceRandom;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(long, short)]
+    #[arg(long, short, value_parser = clap::value_parser!(u8).range(1..))]
     times: u8,
-    #[arg(long, short)]
+    #[arg(long, short, value_parser = clap::value_parser!(u32).range(2..))]
     peoples: u32,
-    #[arg(long, short)]
-    second: u32,
+    #[arg(long, short, value_parser = clap::value_parser!(u32).range(1..))]
+    secs: u32,
 }
 
 fn main() {
     let cli = Args::parse();
     let times = cli.times;
     let peoples = cli.peoples;
-    let second = cli.second;
+    let second = cli.secs;
 
     let mut timer = Instant::now();
-    let mut rng = rand::thread_rng();
+    // let mut rng = rand::thread_rng();
 
     let mut count = 0;
-
     let mut peoples_vec = (1..=peoples).collect::<Vec<_>>();
 
     loop {
@@ -37,7 +32,8 @@ fn main() {
         if times == count {
             break;
         }
-        let mut lucky = None;
+        let mut lucky: u32;
+        let mut index: Option<usize> = None;
         loop {
             if timer.elapsed().as_secs_f32() >= second as f32 {
                 pb.finish();
@@ -46,11 +42,7 @@ fn main() {
                 loop {
                     let stdin = stdin().read_line(&mut buffer);
                     if stdin.is_ok() {
-                        let index = peoples_vec
-                            .iter()
-                            .position(|x| x == lucky.unwrap())
-                            .unwrap();
-                        peoples_vec.remove(index);
+                        peoples_vec.remove(index.unwrap());
                         timer = Instant::now();
                         pb.finish_and_clear();
                         break;
@@ -58,14 +50,11 @@ fn main() {
                 }
                 break;
             }
-            lucky = peoples_vec.choose(&mut rng);
-            if let Some(lucky) = lucky {
-                pb.set_message(format!("Lucky number: {lucky}"));
-            }
+            index = Some(fastrand::usize(..peoples_vec.len()));
+            lucky = peoples_vec[index.unwrap()];
+            pb.set_message(format!("Lucky number: {lucky}"));
         }
 
         count += 1;
     }
-
-    drop(count);
 }
