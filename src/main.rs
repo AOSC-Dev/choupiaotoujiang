5ff43b1afa8e41664ddaf6911e325db1c6a2c1df
eatradish <sakiiily@aosc.io>
2024-07-07 15:35:59 +0800

    feat: add seed feature

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use std::{io::stdin, time::Instant};
+use std::{
+    fs,
+    io::{self, stdin, BufReader},
+    path::PathBuf,
+    time::Instant,
+};
 
 use clap::Parser;
 use indicatif::ProgressBar;
+use sha2::{Digest, Sha512};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+enum App {
+    Seed {
+        file: PathBuf,
+        #[arg(long, short, value_parser = clap::value_parser!(u32).range(2..))]
+        peoples: u32,
+    },
+    Random(Args),
+}
+
+#[derive(Parser, Debug)]
 struct Args {
     #[arg(long, short, value_parser = clap::value_parser!(u8).range(1..))]
     times: u8,
@@ -15,12 +31,25 @@ struct Args {
 }
 
 fn main() {
+    let app = App::parse();
+
+    match app {
+        App::Seed { file, peoples } => {
+            let num = seed_from_file(peoples, file);
+            println!("{num}");
+        }
+        App::Random(args) => {
+            random(args);
+        }
+    }
+}
+
+fn random(args: Args) {
     let Args {
         times,
         peoples,
         secs,
-    } = Args::parse();
-
+    } = args;
     let mut timer = Instant::now();
 
     let mut count = 0;
@@ -49,6 +78,7 @@ fn main() {
                 }
                 break;
             }
+
             index = Some(fastrand::usize(..peoples_vec.len()));
             lucky = peoples_vec[index.unwrap()];
             pb.set_message(format!("Lucky number: {lucky}"));
@@ -56,4 +86,17 @@ fn main() {
 
         count += 1;
     }
+}
+
+fn seed_from_file(peoples: u32, file: PathBuf) -> usize {
+    let f = fs::File::open(file).unwrap();
+    let mut reader = BufReader::new(f);
+    let mut sha512 = Sha512::new();
+    io::copy(&mut reader, &mut sha512).unwrap();
+    let v = sha512.finalize();
+
+    let num: u64 = v.into_iter().map(|x| x as u64).sum();
+    fastrand::seed(num);
+
+    fastrand::usize(1..peoples as usize)
 }
